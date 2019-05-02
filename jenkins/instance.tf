9711a14c1924799a94fb887571bcca87aa719d67
anoop600 <anoop.jain10@gmail.com>
2019-05-02 12:15:27 +0000

    added scripts for jenkins

---
+++ jenkins/instance.tf
@@ -0,0 +1,23 @@
+data "aws_ami" "ec2-ami" {
+  owners           = ["self"]
+
+  filter {
+    name   = "state"
+    values = ["available"]
+  }
+  filter {
+    name   = "tag:Name"
+    values = ["Packer-Jenkins"]
+  }
+  most_recent = true
+}
+
+resource "aws_instance" "web" {
+  ami           = "${data.aws_ami.ec2-ami.id}"
+  instance_type = "t2.small"
+  key_name = "devops-jan"
+  security_groups = ["${aws_security_group.jenkins-security.name}"]
+  tags = {
+    Name = "Packer-Jenkin"
+  }
+}
